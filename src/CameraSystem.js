b5ff95e1c738d644693e423ebfb8c8387cfb9e6d
Georgi Nikoloff <georgi@Georgis-MacBook-Pro.local>
2020-03-31 22:17:30 +0200

    align properly photo next to articles in expanded mode

--- src/CameraSystem.js
+++ src/CameraSystem.js
@@ -7,13 +7,13 @@ import {
   WOLRD_WIDTH,
   WORLD_HEIGHT,
   CAMERA_MIN_VELOCITY_TO_BE_MOVING,
-
   EVT_CAMERA_HANDLE_MOVEMENT_WORLD,
   EVT_CAMERA_ZOOM_OUT_DRAG_START,
   EVT_CAMERA_ZOOM_IN_DRAG_END,
-
   EVT_APP_RESIZE,
   EVT_ON_SCENE_DRAG,
+  EVT_CLOSE_REQUEST_SINGLE_PROJECT,
+  EVT_OPEN_REQUEST_SINGLE_PROJECT,
 } from './constants'
 
 export default class CameraSystem {
@@ -47,6 +47,7 @@ export default class CameraSystem {
     this._isDragCameraMoving = false
     this._zoomFactor = 1
     this._isZoomedOut = false
+    this._shouldMove = true
 
     const cameraLookAt = new THREE.Vector3(0, 0, 0)
 
@@ -67,6 +68,8 @@ export default class CameraSystem {
     this._postFXCamera.position.copy(position)
     this._postFXCamera.lookAt(cameraLookAt)
 
+    eventEmitter.on(EVT_OPEN_REQUEST_SINGLE_PROJECT, this._onRequestOpenProject)
+    eventEmitter.on(EVT_CLOSE_REQUEST_SINGLE_PROJECT, this._onRequestCloseProject)
     eventEmitter.on(EVT_CAMERA_HANDLE_MOVEMENT_WORLD, this._handleMovement)
     eventEmitter.on(EVT_ON_SCENE_DRAG, this._onSceneDrag)
     eventEmitter.on(EVT_CAMERA_ZOOM_OUT_DRAG_START, this._onDragZoomOut)
@@ -88,7 +91,19 @@ export default class CameraSystem {
   get isDragCameraMoving () {
     return this._isDragCameraMoving
   }
+  _onRequestOpenProject = () => {
+    this._shouldMove = false
+    this._targetPosition.x = this._clipCamera.position.x
+    this._targetPosition.y = this._clipCamera.position.y
+    // this._velocity.set(0, 0, 0)
+  }
+  _onRequestCloseProject = () => {
+    this._shouldMove = true
+  }
   _handleMovement = (ts, dt) => {
+    if (!this._shouldMove) {
+      return
+    }
     let oldCameraVelocityX = this._velocity.x
     let oldCameraVelocityY = this._velocity.y
 
@@ -109,6 +124,8 @@ export default class CameraSystem {
     this._clipCamera.position.y += this._velocity.y
     this._photoCamera.position.x += this._velocity.x
     this._photoCamera.position.y += this._velocity.y
+
+    console.log('updating')
 
     this._clipCamera.position.x  *= CameraSystem.friction
     this._clipCamera.position.y  *= CameraSystem.friction
