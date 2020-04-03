c480d5005621e5804bcae76c420ab62d10ab70ad
Georgi Nikoloff <georgi@Georgis-MacBook-Pro.local>
2020-04-03 02:17:28 +0200

    prototype info section

--- src/InfoPanel.js
+++ src/InfoPanel.js
@@ -3,6 +3,11 @@ import styler from 'stylefire'
 
 import eventEmitter from './event-emitter'
 
+
+import {
+  mapNumber,
+} from './helpers'
+
 import {
   EVT_OPENING_SINGLE_PROJECT,
   EVT_OPEN_SINGLE_PROJECT,
@@ -11,18 +16,23 @@ import {
   EVT_OPENING_INFO_SECTION,
   EVT_OPEN_REQUEST_INFO_SECTION,
 } from './constants'
-import { mapNumber } from './helpers'
 
 export default class InfoPanel {
+  static ESCAPE_KEY_CODE = 27
+
   constructor () {
     const wrapper = document.getElementsByClassName('info-section')[0]
     this.$els = {
       wrapper,
       toggleButton: wrapper.getElementsByClassName('info-btn')[0],
+      closeButton: document.getElementsByClassName('info-btn-close')[0],
+      logo: document.getElementsByClassName('app-logo')[0],
     }
 
     this.stylers = {
       toggleButton: styler(this.$els.toggleButton),
+      closeButton: styler(this.$els.closeButton),
+      logo: styler(this.$els.logo),
     }
 
     eventEmitter.on(EVT_OPENING_SINGLE_PROJECT, ({ tweenFactor }) => {
@@ -41,6 +51,7 @@ export default class InfoPanel {
     })
 
     this.$els.toggleButton.addEventListener('click', this._onOpenRequest, false)
+    this.$els.closeButton.addEventListener('click', this._onCloseRequest, false)
   }
   _onOpenRequest = () => {
     eventEmitter.emit(EVT_OPEN_REQUEST_INFO_SECTION)
@@ -48,10 +59,38 @@ export default class InfoPanel {
       update: tweenFactor => {
         eventEmitter.emit(EVT_OPENING_INFO_SECTION, { tweenFactor })
         this.stylers.toggleButton.set('opacity', 1 - tweenFactor)
+        this.stylers.logo.set('opacity', 1 - tweenFactor)
+        this.stylers.closeButton.set({
+          opacity: tweenFactor,
+          y: mapNumber(tweenFactor, 0, 1, -100, 0),
+        })
       },
       complete: () => {
         this.stylers.toggleButton.set('pointerEvents', 'none')
+        window.addEventListener('keydown', this._onKeyDown, false)
       }
     })
+  }
+  _onCloseRequest = () => {
+    tween().start({
+      update: tweenFactor => {
+        eventEmitter.emit(EVT_OPENING_INFO_SECTION, { tweenFactor: 1 - tweenFactor })
+        this.stylers.toggleButton.set('opacity', tweenFactor)
+        this.stylers.logo.set('opacity', tweenFactor)
+        this.stylers.closeButton.set({
+          opacity: 1 - tweenFactor,
+          y: mapNumber(tweenFactor, 0, 1, 0, -100),
+        })
+      },
+      complete: () => {
+        this.stylers.toggleButton.set('pointerEvents', 'auto')
+        window.removeEventListener('keydown', this._onKeyDown)
+      },
+    })
+  }
+  _onKeyDown = ({ keyCode }) => {
+    if (keyCode === InfoPanel.ESCAPE_KEY_CODE) {
+      this._onCloseRequest()
+    }
   }
 }
