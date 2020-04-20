6c45680525a384d0ded1d6db240948c13452b667
georgi <nikoloffgeorgi@gmail.com>
2020-04-20 12:48:23 +0200

    remove unnecessary logging

--- src/Loader/index.js
+++ src/Loader/index.js
@@ -165,7 +165,6 @@ export default class Loader {
     }, Loader.PROGRESS_TIMER_VALUE)
   }
   _onUpdate = (ts, dt) => {
-    console.log(dt)
     this._loadProgress += (this._loadProgressTarget - this._loadProgress) * (dt * this._dtScale)
     // if (mobileBrowser) {
       
